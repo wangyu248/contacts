ebe77fdc02d269d01615cd6fa45e9b1c067ee021
wangyu248 <2302484042@qq.com>
2020-12-13 16:33:23 +0800

    update about

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="about-container">
-        <h3>Team247联系人管理</h3>
+        <h3>通讯录</h3>
         <p>基于vue+vue-router+vuex+bootstrap-vue+axios</p>
-        <p>作者：张威 2017年8月</p>
+        <p>作者：王雨 刘涵宇 朱熠 郑国强 陈艺东</p>
     </section>
 </template>
 <style lang="less">
